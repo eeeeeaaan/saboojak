05cf4b87b0e594f450a609a61c8e9451461a8e86
eeeeeaaan <wjdwjddla73@naver.com>
2026-02-21 17:08:25 +0900

    [Deployment] 도커 빌드 오류 수정

--- src/main/java/com/example/individual/IndividualProjectApplication.java
+++ src/main/java/com/example/individual/IndividualProjectApplication.java
@@ -2,8 +2,16 @@ package com.example.individual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(
+	basePackages = "com.example.individual.domain" // domain 하위의 모든 jpaRepository 스캔
+)
+@EnableRedisRepositories(
+	basePackages = "com.example.individual.infra.redis" // infra 하위의 redisRepository만 스캔
+)
 public class IndividualProjectApplication {
 
 	public static void main(String[] args) {
